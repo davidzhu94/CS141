027ea6aeef1276a3b4028cb38c1d17665fb370aa
davidzhu94 <davidzhu94@gmail.com>
2016-05-18 02:08:04 -0700

    Started function to create table for dynamic programming

--- SeamCarving.py
+++ SeamCarving.py
@@ -1,14 +1,22 @@
 import sys
+
+holder = []
 def Read_File():
     number_of_args = len(sys.argv)
     file = open(sys.argv[1], "r")
     index = 0
-    holder = []
     for line in file:
-        line.strip("\n")
         line1 = line.split(", ")
         holder.append(line1)
         index += 1
+
+def Create_Table():
+    table = holder[0]
+    print float(holder[0][len(holder[0])-1]) + float(holder[0][len(holder[0])-1])
+    print table
+
+Read_File()
+Create_Table()
 
 def Write_to_File(s):
     inputName = sys.argv[1][:len(sys.argv[1])-4]
