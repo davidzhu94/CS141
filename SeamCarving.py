7ded1add4a72a3f00f3649df99b74583e90df995
davidzhu94 <davidzhu94@gmail.com>
2016-05-19 01:23:47 -0700

    Deleted unnecessary print statement

--- SeamCarving.py
+++ SeamCarving.py
@@ -16,7 +16,6 @@ def Read_File():
 
 def Create_Table(holder):
     table.append(holder[0])
-    print float(holder[0][len(holder[0])-1]) + float(holder[0][len(holder[0])-1])
     i = 1
     for i in range(1, len(holder)):
         table.append([])
